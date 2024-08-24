e71ecd91bb054d5373327a1e6d3165c960c10fc3
parker <4215272-pdamon@users.noreply.gitlab.com>
2024-08-24 11:10:26 -0600

    delete unused plugins

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -4,18 +4,14 @@ return {
   config = function()
     require('toggleterm').setup {
       -- Additional configuration options
-      size = 20,
       hide_numbers = true,
       shade_filetypes = {},
       shade_terminals = true,
       shading_factor = 2,
       start_in_insert = true,
       persist_size = true,
-      direction = 'float',
+      direction = 'tab',
       close_on_exit = true,
-      float_opts = {
-        title_pos = 'center',
-      },
       winbar = {
         enabled = true,
         name_formatter = function(term) --  term: Terminal
@@ -29,23 +25,26 @@ return {
     -- Function to create a terminal and run a specific command
     local function term_cmd(cmd, count)
       local Terminal = require('toggleterm.terminal').Terminal
-      local term = Terminal:new { cmd = cmd, hidden = true, count = count, direction = 'float' }
+      local term = Terminal:new { cmd = cmd, hidden = true, count = count, direction = 'tab' }
       return function()
         term:toggle()
       end
     end
 
-    local function toggle_term(count)
+    local function toggle_term(count, directon)
       local Terminal = require('toggleterm.terminal').Terminal
-      local term = Terminal:new { hidden = true, count = count, direction = 'float' }
+      local term = Terminal:new { hidden = true, count = count, direction = directon }
       return function()
         term:toggle()
       end
     end
     -- Setup key mappings
     local map = vim.api.nvim_set_keymap
+    map('n', '<leader>tt', '', { callback = toggle_term(100, 'tab'), noremap = true, silent = true, desc = 'ToggleTerm Tab' })
+    map('n', '<leader>th', '', { callback = toggle_term(101, 'horizontal'), noremap = true, silent = true, desc = 'ToggleTerm Horizontal' })
+    map('n', '<leader>tv', '', { callback = toggle_term(102, 'vertical'), noremap = true, silent = true, desc = 'ToggleTerm Vertical' })
     -- Htop
-    map('n', '<leader>th', '', { callback = term_cmd('htop', 10), noremap = true, silent = true, desc = 'ToggleTerm htop' })
+    -- map('n', '<leader>th', '', { callback = term_cmd('htop', 10), noremap = true, silent = true, desc = 'ToggleTerm htop' })
     -- Lazygit
     map('n', '<leader>tg', '', { callback = term_cmd('lazygit', 20), noremap = true, silent = true, desc = 'ToggleTerm lazygit' })
     -- Python
@@ -53,7 +52,7 @@ return {
 
     for i = 1, 4 do
       map('n', '<leader>t' .. i, '', {
-        callback = toggle_term(i),
+        callback = toggle_term(i, 'tab'),
         noremap = true,
         silent = true,
         desc = 'ToggleTerm ' .. i,
