d1babbe59c0360e66a39a913ef52b31fdb04747a
parker <4215272-pdamon@users.noreply.gitlab.com>
2025-06-06 14:26:52 -0600

    rename ruff, remove go

--- init.lua
+++ init.lua
@@ -691,10 +691,10 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        gopls = {},
+        -- gopls = {},
         pyright = {},
         rust_analyzer = {},
-        ruff_lsp = {},
+        ruff = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
